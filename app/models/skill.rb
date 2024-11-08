66cfd5ec1168b90381daa77cb0a8ea4691eb2d03
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2024-11-08 18:00:16 +0500

    Add validations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,5 @@
 class Skill < ApplicationRecord
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :interests
-  has_and_belongs_to_many :skills
+  has_and_belongs_to_many :interests, dependent: :destroy
+  has_and_belongs_to_many :skills, dependent: :destroy
 
   validates :age, :country, :email, :gender, :name, :nationality, :patronymic, :surname, presence: true
-  validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 90 }
+  validates :age, numericality: { greater_than: 0, less_than_or_equal_to: 90 }
   validates :gender, inclusion: { in: %w[male female] }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
